697f783777791a51f0c87fa6d6db1b6f6cfeb0f9
Mana-One <paolo.manaois@yahoo.fr>
2021-04-07 16:11:58 +0200

    changed formula for speed

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,27 @@
+type NatureBonus = 1 | 1.1 | 0.9;
+export interface Nature {
+    attack: NatureBonus;
+    defense: NatureBonus;
+    speed:NatureBonus;
+}
+
 export interface PokemonProps {
     name: string;
-    speed: number;
+    baseSpeed: number;
+    baseAttack: number;
+    level: number;
+    nature: Nature;
 }
 
 export class Pokemon implements PokemonProps {
     readonly name: string;
-    readonly speed: number;
+    readonly baseSpeed: number;
+    readonly baseAttack: number;
+    readonly nature: Nature;
+    level: number;
 
     static getFirstAttacker (pokemon1: Pokemon, pokemon2: Pokemon): Pokemon {
-        if (pokemon1.speed >= pokemon2.speed){
+        if (pokemon1.getSpeed() >= pokemon2.getSpeed()){
             return pokemon1;
         }
         return pokemon2;
@@ -16,6 +29,23 @@ export class Pokemon implements PokemonProps {
 
     constructor (props: PokemonProps) {
         this.name = props.name;
-        this.speed = props.speed;
+        this.baseSpeed = props.baseSpeed;
+        this.baseAttack = props.baseAttack;
+        this.nature = props.nature;
+        this.level = props.level;
+    }
+
+    private getFinalStatValue (baseValue: number, statName: string): number {
+        return Math.floor(
+            Math.floor(2 * baseValue * this.level / 100 + 5) * this.nature[statName]
+        );
+    }
+
+    getSpeed(): number {
+        return this.getFinalStatValue(this.baseSpeed, "speed");
+    }
+
+    getAttackDamage(): number {
+        return this.getFinalStatValue(this.baseAttack, "attack");
     }
 };
--- src/pokemon.test.ts
+++ src/pokemon.test.ts
@@ -2,16 +2,38 @@ import {Pokemon} from "./index";
 
 const p1 = new Pokemon({
     name: "hoho",
-    speed: 130
+    baseSpeed: 130,
+    baseAttack: 40,
+    level: 53,
+    nature: {
+        attack: 1.1,
+        speed: 0.9,
+        defense: 1
+    }
 });
 
 const p2 = new Pokemon({
     name: "haha",
-    speed: 120
+    baseSpeed: 120,
+    baseAttack: 50,
+    level: 52,
+    nature: {
+        attack: 1,
+        speed: 1.1,
+        defense: 0.9
+    }
 });
 
 describe("Pokemon class", function(){
-    it( "should be faster", function(){
-        expect(Pokemon.getFirstAttacker(p1, p2)).toBe(p1);
+    it( "p1's speed should be 127", function(){
+        expect(p1.getSpeed()).toBe(127);
+    });
+
+    it( "p2's speed should be  141", function(){
+        expect(p2.getSpeed()).toBe(141);
+    })
+
+    it( "p2 should be faster", function(){
+        expect(Pokemon.getFirstAttacker(p1, p2)).toBe(p2);
     });
 })
